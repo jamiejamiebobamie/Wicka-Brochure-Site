a977b4fee2723d50e49f8a2237a71428b3bc3481
jamiejamiebobamie <jmccrory@vt.edu>
2019-09-13 20:24:35 -0700

    edited homepage. need to figure out a style rule to push the character sprite to the left with page width changing.

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,9 +7,6 @@ function Homepage() {
     <div className="Homepage">
         <NavBar />
         <div class="character">
-            <div className="candle_sprite_test"/>
-            <div className="door_sprite"/>
-            <img className="monster_pack" src="/media/imgs/monster_eyes_static.png" width='500' height='80.818' alt="logo" />
             <div className="character_sprite"/>
         </div>
     </div>
@@ -18,3 +15,7 @@ function Homepage() {
 }
 
 export default Homepage
+// <img className="monster_pack" src="/media/imgs/monster_eyes_static.png" width='500' height='80.818' alt="logo" />
+
+// <div className="candle_sprite_test"/>
+// <div className="door_sprite"/>
