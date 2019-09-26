0daa486e360dc12eb6c89be061179c1fa0348345
jamiejamiebobamie <jmccrory@vt.edu>
2019-09-25 19:36:59 -0700

    messing around with scrollsnap

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
     constructor(props){
         super(props)
-        this.state = {width: 0, height: 0}
+        this.state = {width: 0, height: 0, thePosition:0}
         //https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
@@ -21,15 +21,35 @@ class App extends Component {
     componentDidMount() {
       this.updateWindowDimensions();
       window.addEventListener('resize', this.updateWindowDimensions);
+      window.addEventListener('scroll', this.listenToScroll)
+
     }
 
     componentWillUnmount() {
       window.removeEventListener('resize', this.updateWindowDimensions);
+      window.removeEventListener('scroll', this.listenToScroll)
+
     }
 
     updateWindowDimensions() {
       this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
+
+// https://stackoverflow.com/questions/53158796/get-scroll-position-with-reactjs
+listenToScroll = () => {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop
+
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight
+
+  const scrolled = winScroll / height
+
+  this.setState({
+    thePosition: scrolled,
+  })
+}
 
     render(){
         return (
@@ -40,7 +60,8 @@ class App extends Component {
                         <Homepage
                             update={this.updateWindowDimensions}
                             width={this.state.width}
-                            height={this.state.height} />
+                            height={this.state.height}
+                            thePosition={this.state.thePosition} />
                     }
                    />
                   <Route path="/about"
--- src/Homepage.js
+++ src/Homepage.js
@@ -5,7 +5,7 @@ import MonsterEyesContainer from './MonsterEyesContainer'
 class Homepage extends Component {
     constructor(props){
         super(props)
-        this.state = { storedState: true }
+        this.state = { storedState: true, theposition: 0 }
 
         this.toggleNavbar = this.toggleNavbar.bind(this)
     }
@@ -15,50 +15,64 @@ class Homepage extends Component {
     }
 
     render(){
+        console.log(this.props.height < 960,
+            this.state.storedState===true,
+            this.props.width < 1400,
+            this.props.height < 650,
+            this.props.thePosition > .5)
         return (
-          <div className="Homepage">
-
-              <NavBar
-                dropdownMenu={true}
-                width={this.props.width}
-                liftState={this.toggleNavbar} />
-
-              { this.props.height < 960
-                  && this.state.storedState===true
-                  && this.props.width < 1400
-                  || this.props.height < 650
-
-              ?
-
-              ""
-
-              :
-
-              <div className="homepage_bottom">
-              <MonsterEyesContainer
-                  update={this.props.update}
-                  width={this.props.width}
-                  height={this.props.height} />
-
-                  <div className="character">
-                      <div className="character_sprite"/>
-                      <img
-                          className="character_background_img"
-                          src='/media/imgs/character_background.png'
-                          alt=''/>
-                    </div>
-
-              </div>
-            }
-
+         <div className="Homepage" dir="ltr" style={{height: this.props.height*2}}>
+          <div className="container">
+            <div className='snap'><h1>1</h1></div>
+            <div className='snap'><h1>2</h1></div>
+            <div className='snap'><h1>3</h1></div>
+            <div className='snap'><h1>4</h1></div>
+            <div className='snap'><h1>5</h1></div>
           </div>
+        </div>
 
         )
     }
 
 }
-
+//
 
 
 
 export default Homepage
+
+
+// <NavBar
+//   dropdownMenu={true}
+//   width={this.props.width}
+//   liftState={this.toggleNavbar} />
+//
+// {
+//     // this.props.height < 960
+//     // && this.state.storedState===true
+//     // && this.props.width < 1400
+//     // || this.props.height < 650 &&
+//     this.props.thePosition > .5
+//
+// ?
+//
+// ""
+//
+// :
+//
+// <div className="homepage_bottom">
+// <MonsterEyesContainer
+//     update={this.props.update}
+//     width={this.props.width}
+//     height={this.props.height} />
+//
+//     <div className="character">
+//         <div className="character_sprite"/>
+//         <img
+//             className="character_background_img"
+//             src='/media/imgs/character_background.png'
+//             alt=''/>
+//       </div>
+//
+// </div>
+// }
