889cc4e6421222ea5bdae7540437cdcce59caad3
jamiejamiebobamie <jmccrory@vt.edu>
2019-09-29 16:43:55 -0700

    attempting to push to heroku

---
+++ build/precache-manifest.d907a5017cc7da94e00142c1b43d8536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fb9267bbc384e1d8cde629c8e793ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc6e5c60aaaef3c79f8c",
+    "url": "/static/css/main.3eded24e.chunk.css"
+  },
+  {
+    "revision": "e6fa079a41998c12e399",
+    "url": "/static/js/2.b5ddcc9e.chunk.js"
+  },
+  {
+    "revision": "bc6e5c60aaaef3c79f8c",
+    "url": "/static/js/main.ec95be44.chunk.js"
+  },
+  {
+    "revision": "a5fa9570a34377e3fe3e",
+    "url": "/static/js/runtime~main.a1b2ca21.js"
+  }
+]);
